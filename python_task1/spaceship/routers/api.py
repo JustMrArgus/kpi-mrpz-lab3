13e2c4973b672c5e069e2464cd8f8714c0c9c02a
JustMrArgus <argusprogramming@protonmail.com>
2025-05-14 02:52:55 +0300

    Add matrix route

--- python_task1/spaceship/routers/api.py
+++ python_task1/spaceship/routers/api.py
@@ -6,3 +6,14 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get("/multimatrix")
+def multimatrix():
+    matrix_a = np.random.rand(10, 10)
+    matrix_b = np.random.rand(10, 10)
+    product = np.dot(matrix_a, matrix_b)
+    return {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist(),
+    }
